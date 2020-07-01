b38b7fa9b677df286d3efc77694a150b26920de4
forsigner <forsigner@gmail.com>
2020-07-01 23:13:46 +0800

    chore: upgrade tiejs

--- 06-typeorm/user/user.service.ts
+++ 06-typeorm/user/user.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@tiejs/common'
-import { InjectRepository } from '@tiejs/typeorm'
 import { UserRepository } from './user.repository'
 
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository() private userRepository: UserRepository) {}
+  constructor(private userRepository: UserRepository) {}
 
   async queryUser() {
-    const users = await this.userRepository.findAll()
-    return users
+    return await this.userRepository.findAll()
   }
 }
